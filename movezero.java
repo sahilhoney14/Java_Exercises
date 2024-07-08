14fb7dfa7f6b938504748ba894c329874463934c
Sahil Singh <145000201+sahilhoney14@users.noreply.github.com>
2024-07-08 21:59:25 +0530

    Add files via upload

---
+++ movezero.java
@@ -0,0 +1,20 @@
+//Given an integer array arr:move all 0's to the end of its while maintaining the relative order of the non-zero elements.
+
+public class movezero {
+    public static void main(String[] args) {
+        int[] arr = {0, 1, 10, 5, 0, 4};
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] == 0 && arr[j + 1] != 0) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        for (int val : arr) {
+            System.out.print(val + " ");
+        }
+    }
+}
